e600caa1537f221a23215ad12482598139dcc9d1
guoliu <liuguomailbox@gmail.com>
2016-07-25 11:02:54 -0700

    new arc

--- plot2.js
+++ plot2.js
@@ -3,8 +3,7 @@
 
     // draw countries
     var country = g
-    .append("g")
-    .selectAll("path")
+    .selectAll(".countries")
     .data(topojson.feature(topology, topology.objects.countries).features)
     .enter()
     .append("path")
@@ -69,7 +68,7 @@
             return Math.round(datum).toLocaleString('en');}
             else{return "no data"}
           }
-        catch(err) { return "no data";} 
+        catch(err) { return "no data"} 
       }
 
       g.selectAll(".countries")
@@ -88,7 +87,10 @@
         d3.select(this).style('fill', colorid);
         tooltipMap.classed('hidden', true);
       })
-      .attr("d", path);;
+      .on('click', function(d) {
+        time_series(result.cleaned, [d.id, -1])
+      })
+      .attr("d", path);
     }
 
     var numeNm = numeList[0],
@@ -96,13 +98,14 @@
     var result = calculate(raw, numeNm, denoNm)
     update(result.cleaned, result.colormap, 2009);
 
-    time_series(result.cleaned, [-1,76], '#time-series')
+    time_series(result.cleaned, [76, -1])
 
     d3.select("#deno").on("change", function(d){
       denoSwitch = -denoSwitch; 
       denoNm = denoList[(denoSwitch+0.5)|0];
       result = calculate(raw, numeNm, denoNm);
       update(result.cleaned, result.colormap, 2009);
+      time_series(result.cleaned, [76, -1]);
     });
 
 
@@ -127,78 +130,153 @@
   });
  }
 
- function time_series(cleaned, ids, div_name) {
+ function time_series(cleaned, ids) {
+  var outerRadius = 240,
+  innerRadius = 205;
+  gap = 2;
+  // width = 560, //- margin.left - margin.right,
+  // height = 560; //- margin.top - margin.bottom;
 
-  // get data used in time series plot
-  var series = [];
-  ids.forEach(function(id) {series.push(cleaned.get(id))});
+  var bandwidth = (outerRadius - innerRadius)/ids.length
 
-  // get total year and value range
-  var values = [], years = [];
-  series.forEach( function(d) {
+  // aggregate all data
+  var layers = [], values = [], years = [];
+  ids.forEach(function(id) {
+    var d = cleaned.get(id);
     d.data.forEach(function(y, v) {
       values.push(v.value);
       years.push(y);
-    } )
-  } );
+    });
+  });
 
   // calculate colormap
   var colormap = d3.scale
   .quantize()
-  .domain(d3.extent(values))
+  .domain([0, d3.max(values)])
   .range(colors);
 
+  ids.forEach(function(id, index) {
+    var d = cleaned.get(id),
+    bandExtent = d3.extent(d.data.values(), function(d) {return d.value});
 
-width = 960; //- margin.left - margin.right,
-height = 69; //- margin.top - margin.bottom;
+    colors.forEach(function(color) {
+      var domain = colormap.invertExtent(color); 
+      if ( (bandExtent[0]<domain[1])|(bandExtent[1]>domain[0]) ) {
+        var layer = {
+          data: d.data,
+          name: d.name,
+          y0: innerRadius + index*bandwidth + index*gap,
+          domain: domain,
+          color: color
+        };
+        layers.push(layer);
+      }
+    });
+  });
 
-var x = d3.time.scale()
-.range([0, width])
-.domain(d3.extent(years));
+  var angle = d3.scale.linear()
+  .range([0, 2 * Math.PI])
+  .domain(d3.extent(years))
+  .clamp(true);
 
-var y = d3.scale.linear()
-.range([height, 0])
-.clamp(true);
+  var radius = d3.scale.linear()
+  .clamp(true);
 
-var area = d3.svg.area()
-.x(function(d) { return x(d.year); })
-.y0(height)
-.y1(function(d) { return y(d.value); });
+  var area = d3.svg.area.radial()
+  .interpolate("cardinal-closed")
+  .angle(function(d) {return angle(d.year); })
+  .outerRadius(function(d) {return radius(d.value)});
 
-var svg = d3.select(div_name).selectAll('svg')
-.data(series)
-.enter().append("svg")
-.attr("width", width)
-.attr("height", height)
-.append("g");
-//.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var line = d3.svg.line.radial()
+  .interpolate("cardinal-closed")
+  .angle(function(d) { return angle(d.time); })
 
-// Add the area path elements. Note: the y-domain is set per element.
-svg.each(function (d) {
-  d3.select(this)
-  .selectAll('svg')
-  .data(colors)
+  svg.selectAll(".time-layer").remove()
+
+  var time_group = svg.append("g")
+  .attr("transform", "translate(" + width/2 + "," + height / 2 + ")");
+
+  
+  // .append("svg")
+  // .attr("width", width)
+  // .attr("height", height)
+
+  time_group.selectAll(".time-layer")
+  .data(layers)
   .enter()
   .append("path")
-  .attr("class", "area")
-  .attr("d", function(c) { 
-    y.domain(colormap.invertExtent(c));
-    return area(d.data.values()); })
-  .attr("fill", function(c) {return c});
-});
+  .attr("class", "time-layer")
+  .attr("d", function(d) {
+    radius
+    .range([d.y0, d.y0 + bandwidth])
+    .domain(d.domain);
 
-// Add a small label for the symbol name.
-svg.append("text")
-.attr("x", 60)
-.attr("y", height - 6)
-.style("text-anchor", "start")
-.text(function(d) { return d.name; });
+    area
+    .innerRadius(d.y0);
+
+    return area(d.data.values()); } )
+  .attr("fill", function(d) {return d.color});
+
+// invisable layer for mouseover
+  var invertCord = function (cord) {
+    var a = Math.atan(-cord[0]/cord[1]);
+    if (cord[1]>0) {
+      return a + Math.PI;
+    }
+    else {
+      if (cord[0]*cord[1]>0) {
+        return a + 2*Math.PI;
+      }
+      else {
+        return a;
+      }
+    }
+  };
+
+  time_group
+  .append("path")
+  .attr("class", "base-layer")
+  .attr("id", "base")
+  .attr("d", d3.svg.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
+    .startAngle(0)
+    .endAngle(2 * Math.PI)(outerRadius - innerRadius))
+  .attr("fill", "transparent")
+  .on('mousemove', function () {
+   cord = d3.mouse(this);
+   r_angle = invertCord(cord);
+   //console.log(d3.svg.line.radial()([[innerRadius, r_angle], [outerRadius, r_angle]]));
+   r_year = Math.round(angle.invert(r_angle));
+
+   time_group.selectAll(".hover-ruler").remove();
+   
+   time_group
+   .append("path")
+   .attr("class", "hover-ruler")
+   .attr("d", d3.svg.line.radial()([[innerRadius, r_angle], [outerRadius, r_angle]]))
+   .attr("fill", "black")
+   .attr('stroke', "black")
+   .attr('stroke-width', 1);
+
+   time_group
+   .append("text")
+   .text(r_year)
+   .attr("class", "hover-ruler")
+   .append("textPath") //append a textPath to the text element
+   .attr("xlink:href", "#base")
+   .attr("startOffset", "50%");
+   //.attr("text-anchor", "middle")
+
+ })
+  .on('mouseout', function () {
+    time_group.selectAll(".hover-ruler").remove();
+  });
 
 function type(d) {
-  d.price = +d.price;
+  d.price =+ d.price;
   d.date = parseDate(d.date);
   return d;
 }
-
 
 }
